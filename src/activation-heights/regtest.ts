142836b111290afa77c40ef55c77dda9439317bf
José Dahlquist <jose.dahlquist@gmail.com>
2021-09-08 13:25:30 -0300

    Change tslint for eslint

--- src/activation-heights/regtest.ts
+++ src/activation-heights/regtest.ts
@@ -2,7 +2,8 @@ import { NetworkUpgradesActivationHeights } from './network-activation-heights';
 import { NetworkUpgrades } from '../enums/network-upgrades';
 
 export class Regtest implements NetworkUpgradesActivationHeights {
-  getActivationHeight(networkUpgrade: NetworkUpgrades): number {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getActivationHeight(_networkUpgrade: NetworkUpgrades): number {
     return 0;
   }
 }
